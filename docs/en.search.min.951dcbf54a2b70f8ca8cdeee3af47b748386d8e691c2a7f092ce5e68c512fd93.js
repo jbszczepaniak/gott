501d2122c2996e03e33e7f147adcfca6abd6139c
Jędrzej Szczepaniak <jbszczepaniak@gmail.com>
2019-11-19 21:11:32 +0100

    deploy

---
+++ docs/en.search.min.951dcbf54a2b70f8ca8cdeee3af47b748386d8e691c2a7f092ce5e68c512fd93.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/gott/flexsearch.min.js');loadScript('/gott/en.search-data.min.cc625bf151af1cdd590a296320a8097fd1c0ca4cd71a5bcca7e6c537f4a837f7.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
